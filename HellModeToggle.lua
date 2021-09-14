02b315c60a43c09b5517c5b05f5bf2953ca222c8
cgull <cgull.uploads@gmail.com>
2021-09-14 11:29:07 -0400

    Initial commit of mod tool, no interactivity yet

---
+++ HellModeToggle.lua
@@ -0,0 +1,50 @@
+--[[
+    HellModeToggle
+    Author:
+        cgull (Discord: cgull#4469)
+
+    Provides functionality for other mods to toggle hell mode
+]]
+
+ModUtil.RegisterMod("HellModeToggle")
+
+local config = {
+    Enabled = true,
+    EnforceRequirements = true, -- enforces 5 heat minimum on Hell Mode
+}
+HellModeToggle.config = config
+
+function HellMode.ToggleHellMode()
+    local currentHellModeState = GameState.Flags.HardMode
+
+    -- adjusting game properties to match
+    if currentHellModeState then
+        GameState.Flags.HardMode = false
+
+        -- turn off Personal Liability pact option
+        GameState.MetaUpgrades[NoInvulnerabilityShrineUpgrade] = 0
+
+        -- adjust bounties
+        for weaponName, weaponRecords in pairs(GameState.RecordClearedShrineThreshold) do
+            for roomName, roomWeaponRecord in pairs(weaponRecords) do
+                GameState.RecordClearedShrineThreshold[weaponName][roomName]  = roomWeaponRecord - 5
+            end
+        end
+    else
+        -- turn on forced Hell Mode pact options
+        for name, amount in pairs( HeroData.DefaultHero.HardModeForcedMetaUpgrades ) do
+            if GameState.MetaUpgrades[name] < amount then
+			    GameState.MetaUpgrades[name] = amount
+            end
+		end
+
+        -- adjust bounties
+        for weaponName, weaponRecords in pairs(GameState.RecordClearedShrineThreshold) do
+            for roomName, roomWeaponRecord in pairs(weaponRecords) do
+                GameState.RecordClearedShrineThreshold[weaponName][roomName]  = roomWeaponRecord + 5
+            end
+        end
+
+        GameState.Flags.HardMode = true
+    end
+end
