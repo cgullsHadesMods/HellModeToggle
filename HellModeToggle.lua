89429779afa365031b74cc9f79090c5ada2324a5
cgull <cgull.uploads@gmail.com>
2021-09-14 14:07:45 -0400

    Making it actually work nicely

--- HellModeToggle.lua
+++ HellModeToggle.lua
@@ -13,16 +13,15 @@ local config = {
 }
 HellModeToggle.config = config
 
-function HellMode.ToggleHellMode()
+function HellModeToggle.ToggleHellMode()
     if HellModeToggle.config.Enabled then
         local currentHellModeState = GameState.Flags.HardMode
 
         -- adjusting game properties to match
         if currentHellModeState then
-            GameState.Flags.HardMode = false
 
             -- turn off Personal Liability pact option
-            GameState.MetaUpgrades[NoInvulnerabilityShrineUpgrade] = 0
+            GameState.MetaUpgrades["NoInvulnerabilityShrineUpgrade"] = nil
 
             -- adjust bounties
             for weaponName, weaponRecords in pairs(GameState.RecordClearedShrineThreshold) do
@@ -30,13 +29,20 @@ function HellMode.ToggleHellMode()
                     GameState.RecordClearedShrineThreshold[weaponName][roomName]  = roomWeaponRecord - 5
                 end
             end
+            GameState.Flags.HardMode = false
         else
             -- turn on forced Hell Mode pact options
-            for name, amount in pairs( HeroData.DefaultHero.HardModeForcedMetaUpgrades ) do
-                if GameState.MetaUpgrades[name] < amount then
-                    GameState.MetaUpgrades[name] = amount
+            for upgradeName, amount in pairs( HeroData.DefaultHero.HardModeForcedMetaUpgrades ) do
+                local upgradeData = MetaUpgradeData[upgradeName]
+                if GameState.MetaUpgrades[upgradeName] ~= nil then
+                    while GameState.MetaUpgrades[upgradeName] < amount do
+                        IncrementTableValue(GameState.MetaUpgrades, upgradeName)
+                        ApplyMetaUpgrade( upgradeData, true, GameState.MetaUpgrades[upgradeName] <= 0, true )
+                    end
                 end
             end
+            GameState.MetaUpgrades["NoInvulnerabilityShrineUpgrade"] = 1
+
 
             -- adjust bounties
             for weaponName, weaponRecords in pairs(GameState.RecordClearedShrineThreshold) do
@@ -47,5 +53,7 @@ function HellMode.ToggleHellMode()
 
             GameState.Flags.HardMode = true
         end
+        GameState.SpentShrinePointsCache = GetTotalSpentShrinePoints()
+        UpdateActiveShrinePoints()
     end
 end
